de3d105e90db3e1414345c400fe582674d5026a9
Muhamad Usri Yusron <muhamad.usri@mhs.itenas.ac.id>
2025-02-13 16:34:13 +0700

    perubahan huruf kapital daily

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         </div>
         <div className="text-center px-6 lg:px-8">
           <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-            Muslim daily 
+            Muslim Daily 
           </h1>
           <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
           Dan carilah pada apa yang telah dianugerahkan Allah kepadamu (kebahagiaan) negeri akhirat, dan janganlah kamu melupakan bagianmu 
